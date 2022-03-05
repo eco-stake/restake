56d9ad30033faa259265a2956004c72076275033
Tom Beynon <tom@tombeynon.co.uk>
2022-03-05 02:46:31 +0000

    Tidy up and improve autostake script

--- src/utils/Helpers.mjs
+++ src/utils/Helpers.mjs
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 export function mapAsync(array, callbackfn) {
   return Promise.all(array.map(callbackfn));
 }
@@ -12,4 +14,11 @@ export function filterAsync(array, callbackfn) {
   return mapAsync(array, callbackfn).then(filterMap => {
     return array.filter((value, index) => filterMap[index]);
   });
+}
+
+export async function executeSync(calls, count){
+  const batchCalls = _.chunk(calls, count);
+  for (const batchCall of batchCalls) {
+    await mapAsync(batchCall, call => call())
+  }
 }
