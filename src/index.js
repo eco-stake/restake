9c8a89d98c16cdbe8920c24206d93d136f83d2d5
Tom Beynon <tom@tombeynon.co.uk>
2022-04-08 14:03:37 +0100

    Move Bugsnag key to env to avoid tracking forks

--- src/index.js
+++ src/index.js
@@ -13,29 +13,40 @@ import NetworkFinder from './components/NetworkFinder'
 import './index.css';
 import reportWebVitals from './utils/reportWebVitals';
 
-Bugsnag.start({
-  apiKey: '5cda10bb1c98f351cd0b722a1535d8c2',
-  plugins: [new BugsnagPluginReact()],
-  enabledReleaseStages: [ 'production', 'staging' ],
-  releaseStage: process.env.NODE_ENV
-})
+const app = (
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NetworkFinder />} />
+        <Route path="/:network" element={<NetworkFinder />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
 
-const ErrorBoundary = Bugsnag.getPlugin('react')
-  .createErrorBoundary(React)
-
-ReactDOM.render(
-  <ErrorBoundary>
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NetworkFinder />} />
-          <Route path="/:network" element={<NetworkFinder />} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
-  </ErrorBoundary>,
-  document.getElementById('root')
-);
+if (process.env.BUGSNAG_KEY) {
+  Bugsnag.start({
+    apiKey: process.env.BUGSNAG_KEY,
+    plugins: [new BugsnagPluginReact()],
+    enabledReleaseStages: ['production', 'staging'],
+    releaseStage: process.env.NODE_ENV
+  })
+  
+  const ErrorBoundary = Bugsnag.getPlugin('react')
+    .createErrorBoundary(React)
+  
+  ReactDOM.render(
+    <ErrorBoundary>
+      {app}
+    </ErrorBoundary>,
+    document.getElementById('root')
+  );
+}else{
+  ReactDOM.render(
+    app,
+    document.getElementById('root')
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
