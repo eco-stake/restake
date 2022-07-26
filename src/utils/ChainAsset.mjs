407a573296ea344b927a1a74bb4aca3f74147874
Tom Beynon <tom@tombeynon.co.uk>
2022-07-26 01:11:02 +0100

    Fix for missing asset data

--- src/utils/ChainAsset.mjs
+++ src/utils/ChainAsset.mjs
@@ -2,7 +2,7 @@ function ChainAsset(data) {
   const { symbol } = data
   const base = data.denom_units.find(el => el.denom === data.base)
   const display = data.denom_units.find(el => el.denom === data.display)
-  const decimals = display.exponent ?? 6
+  const decimals = display?.exponent ?? 6
   const logo_URIs = data.logo_URIs
   const image = logo_URIs && (logo_URIs.svg || logo_URIs.png)
 
