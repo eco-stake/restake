22b1a6f7116e4f5e9a375e4dc69302ceaeddb744
Keefer Taylor <keefer@keefertaylor.com>
2022-08-14 01:41:52 -0700

    attempt 2

--- scripts/autostake.mjs
+++ scripts/autostake.mjs
@@ -1,5 +1,26 @@
 import { Autostake } from "./base.mjs";
 
 const autostake = new Autostake();
-const networkName = process.argv.slice(2, process.argv.length)
-autostake.run(networkName)
+const networkNames = process.argv.slice(2, process.argv.length)
+
+const main = async () => {
+  const RETRIES = 10
+  for (let i = 0; i < networkNames; i++) {
+    const networkName = networkNames[i]
+    console.log(`Starting Restake for ${networkName}...`)
+
+    for (let j = 0; j < RETRIES; j++) {
+      try {
+        await autostake.run(networkName)
+
+        // Success! Set j to mx to short circuit the loop.
+        j = tries
+      } catch (e) {
+        console.log(`[${i + 1}/${RETRIES}]Caught an error running on ${networkName}: ${e}`)
+        console.log(`Retrying after a delay...`)
+        await new Promise(r => setTimeout(r, (Math.random() * 31) * 1000));
+      }
+    }
+  }
+}
+main()
