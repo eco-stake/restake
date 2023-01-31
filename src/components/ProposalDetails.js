2332c550aa06c8eccabd152bd7cf6fbdd9385074
Tom Beynon <tom@tombeynon.co.uk>
2023-01-31 20:24:40 +0000

    Handle no proposal description

--- src/components/ProposalDetails.js
+++ src/components/ProposalDetails.js
@@ -20,7 +20,7 @@ function ProposalDetails(props) {
   const { proposal_id, content } = proposal
   const { title, description } = content
 
-  const fixDescription = description.replace(/\/n/g, '\n').split(/\\n/).join('\n')
+  const fixDescription = description?.replace(/\/n/g, '\n').split(/\\n/).join('\n')
 
   useEffect(() => {
     if(props.address !== props.wallet?.address && props.granters.includes(props.address)){
