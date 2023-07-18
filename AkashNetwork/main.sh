6cf486ec2b8bd26444d9ebdaee7251a2ed572d1c
Dimokus <Dimokus@mail.ru>
2023-07-18 09:49:26 +0300

    Update main.sh

--- AkashNetwork/main.sh
+++ AkashNetwork/main.sh
@@ -3,13 +3,14 @@
 # Discord Dimokus_
 # 2023
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt install -y build-essential git make gcc nvme-cli nodejs 
+apt install -y build-essential git make gcc nvme-cli nodejs ssh
 node --version
 npm --version
 sleep 5
+if [[ -n $SSH_PASS ]] ; then apt install -y ssh; echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && (echo ${SSH_PASS}; echo ${SSH_PASS}) | passwd root && service ssh restart; fi
 git clone https://github.com/eco-stake/restake
 cd restake
 npm install
-echo $MNEMONIC > .env
+echo MNEMONIC="$MNEMONIC" > .env
 npm run autostake
 sleep infinity
