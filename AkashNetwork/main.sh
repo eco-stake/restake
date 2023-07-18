b22e42455fb02018a2f9fb13422d8e526668131e
Dimokus <Dimokus@mail.ru>
2023-07-18 12:14:42 +0300

    Update main.sh

--- AkashNetwork/main.sh
+++ AkashNetwork/main.sh
@@ -2,7 +2,7 @@
 # Telegram @Dimokus
 # Discord Dimokus_
 # 2023
-apt install -y nano build-essential git make gcc nvme-cli ssh cron
+apt install -y nano wget build-essential git make gcc nvme-cli ssh cron
 export EDITOR=nano
 curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
 # read the script file and when you're sure it's safe run it
@@ -16,7 +16,9 @@ if [[ -n $SSH_PASS ]] ; then apt install -y ssh; echo "PermitRootLogin yes" >> /
 git clone https://github.com/eco-stake/restake
 cd restake
 npm install
-echo MNEMONIC="$MNEMONIC" > .env
+if [[ -n $MNEMONIC ]]; then echo MNEMONIC="$MNEMONIC" > .env;fi
+if [[ -n $MNEMONIC_BASE64 ]]; then echo MNEMONIC=`echo "$MNEMONIC_BASE64" | base64 -d` > .env;fi
+if [[ -n $NETWORK_JSON_LINK ]] ; then wget -O /restake/src/networks.json $NETWORK_JSON_LINK;fi
 npm run autostake
 crontab -l > current_cron
 echo "0 12 * * * /bin/bash -c 'cd /restake && npm run autostake' > /restake/restake.log 2>&1" >> ~/current_cron
