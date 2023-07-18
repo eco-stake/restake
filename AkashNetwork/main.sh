99e772949ddb6561b0161188b5cbfd9e22e2cac1
Dimokus <Dimokus@mail.ru>
2023-07-18 10:26:07 +0300

    Update main.sh

--- AkashNetwork/main.sh
+++ AkashNetwork/main.sh
@@ -3,7 +3,8 @@
 # Discord Dimokus_
 # 2023
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt install -y build-essential git make gcc nvme-cli ssh
+apt install -y nano build-essential git make gcc nvme-cli ssh cron
+export EDITOR=nano
 curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
 # read the script file and when you're sure it's safe run it
 chmod +x /tmp/nodesource_setup.sh
@@ -18,4 +19,8 @@ cd restake
 npm install
 echo MNEMONIC="$MNEMONIC" > .env
 npm run autostake
+crontab -l > current_cron
+echo "0 21 * * * /bin/bash -c "cd restake && npm run autostake" > ./restake.log 2>&1 " >> ~/current_cron
+crontab -l | cat - ~/current_cron | crontab -
+service cron start
 sleep infinity
