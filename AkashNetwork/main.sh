82c386d252cf20b552a79eca1b8622a04add21d1
Dimokus <Dimokus@mail.ru>
2023-07-18 10:00:33 +0300

    Update main.sh

--- AkashNetwork/main.sh
+++ AkashNetwork/main.sh
@@ -3,7 +3,12 @@
 # Discord Dimokus_
 # 2023
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt install -y build-essential git make gcc nvme-cli nodejs npm ssh
+apt install -y build-essential git make gcc nvme-cli ssh
+curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
+# read the script file and when you're sure it's safe run it
+chmod +x /tmp/nodesource_setup.sh
+/tmp/nodesource_setup.sh
+apt install nodejs -y
 node --version
 npm --version
 sleep 5
