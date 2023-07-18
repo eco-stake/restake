2a54add2e45623e38265f1783c2a372165219270
Dimokus <Dimokus@mail.ru>
2023-07-18 12:05:24 +0300

    Update main.sh

--- AkashNetwork/main.sh
+++ AkashNetwork/main.sh
@@ -19,7 +19,7 @@ npm install
 echo MNEMONIC="$MNEMONIC" > .env
 npm run autostake
 crontab -l > current_cron
-echo "0 9 * * * /bin/bash -c 'cd /restake && npm run autostake' > /restake/restake.log 2>&1" >> ~/current_cron
+echo "0 12 * * * /bin/bash -c 'cd /restake && npm run autostake' > /restake/restake.log 2>&1" >> ~/current_cron
 crontab -l | cat - ~/current_cron | crontab -
 service cron start
 sleep infinity
