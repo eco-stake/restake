d73b7faba67a205a007e3ff6ab30da00fa3be891
Dimokus <Dimokus@mail.ru>
2023-07-19 22:28:24 +0300

    Update main.sh

--- AkashNetwork/main.sh
+++ AkashNetwork/main.sh
@@ -11,13 +11,12 @@ chmod +x /tmp/nodesource_setup.sh
 apt install nodejs -y
 node --version
 npm --version
-sleep 5
 if [[ -n $SSH_PASS ]] ; then apt install -y ssh; echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && (echo ${SSH_PASS}; echo ${SSH_PASS}) | passwd root && service ssh restart; fi
 git clone https://github.com/eco-stake/restake
 cd restake
 npm install
 if [[ -n $MNEMONIC ]]; then echo MNEMONIC="$MNEMONIC" > .env;fi
-if [[ -n $MNEMONIC_BASE64 ]]; then echo MNEMONIC=`echo "$MNEMONIC_BASE64" | base64 -d` > .env;fi
+if [[ -n $MNEMONIC_BASE64 ]]; then echo MNEMONIC=$("$MNEMONIC_BASE64" | base64 -d) > .env;fi
 if [[ -n $NETWORK_JSON_LINK ]] ; then wget -O /restake/src/networks.json $NETWORK_JSON_LINK;fi
 if [[ -n $NETWORK_LOCAL_JSON_LINK ]] ; then wget -O /restake/src/networks.local.json $NETWORK_LOCAL_JSON_LINK;fi
 npm run autostake
