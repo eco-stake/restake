31276c2d2a83fed93f764942e6a5713bb70f6088
Dimokus <Dimokus@mail.ru>
2023-07-20 11:40:36 +0300

    Update main.sh

--- AkashNetwork/main.sh
+++ AkashNetwork/main.sh
@@ -15,12 +15,12 @@ if [[ -n $SSH_PASS ]] ; then apt install -y ssh; echo "PermitRootLogin yes" >> /
 git clone https://github.com/eco-stake/restake
 cd restake
 npm install
-if [[ -n $MNEMONIC ]]; then echo MNEMONIC="$MNEMONIC" > .env;fi
-if [[ -n $MNEMONIC_BASE64 ]]; then echo MNEMONIC=`echo "$MNEMONIC_BASE64" | base64 -d` > .env;fi
-if [[ -n $NETWORK_JSON_LINK ]] ; then wget -O /restake/src/networks.json $NETWORK_JSON_LINK;fi
-if [[ -n $NETWORK_LOCAL_JSON_LINK ]] ; then wget -O /restake/src/networks.local.json $NETWORK_LOCAL_JSON_LINK;fi
+if [[ -n $MNEMONIC ]]; then echo MNEMONIC="$MNEMONIC" > .env; fi
+if [[ -n $MNEMONIC_BASE64 ]]; then echo MNEMONIC=`echo "$MNEMONIC_BASE64" | base64 -d` > .env; fi
+if [[ -n $NETWORK_JSON_LINK ]]; then wget -O /restake/src/networks.json $NETWORK_JSON_LINK;fi
+if [[ -n $NETWORK_LOCAL_JSON_LINK ]]; then wget -O /restake/src/networks.local.json $NETWORK_LOCAL_JSON_LINK; fi
 
-if [[ -n $RUN_ARG]];then alias autostake='npm run autostake $RUN_ARG';else alias autostake='npm run autostake';fi
+if [[ -n $RUN_ARG ]]; then alias autostake='npm run autostake $RUN_ARG'; else alias autostake='npm run autostake'; fi
 autostake
 crontab -l > current_cron
 echo "$CRONTAB /bin/bash -c 'cd /restake && autostake' > /restake/restake.log 2>&1" >> ~/current_cron
